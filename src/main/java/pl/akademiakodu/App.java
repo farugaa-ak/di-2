59059720c106d88ce491933b6a7c36b50aef8e51
Artur Faruga <farugaa.training@gmail.com>
2017-11-13 11:14:41 +0100

    to hard worker2

--- src/main/java/pl/akademiakodu/App.java
+++ src/main/java/pl/akademiakodu/App.java
@@ -3,6 +3,8 @@ package pl.akademiakodu;
 import pl.akademiakodu.model.Company;
 import pl.akademiakodu.model.HardWorker;
 import pl.akademiakodu.model.LazyWorker;
+import pl.akademiakodu.model.SmartWorker;
+import pl.akademiakodu.model.Worker;
 
 /**
  * Hello world!
@@ -10,9 +12,14 @@ import pl.akademiakodu.model.LazyWorker;
  */
 public class App 
 {
-    public static void main( String[] args )
+
+    public static void main(String[] args )
     {
-        Company company = new Company("Some company", new HardWorker());
+        //dependency injection
+        SmartWorker smartWorker = new SmartWorker();
+        Worker worker = new HardWorker();
+
+        Company company = new Company("Some company", worker);
         company.startWork();
     }
 }
